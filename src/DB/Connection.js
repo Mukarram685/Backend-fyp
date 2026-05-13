9d16d0eb5137de7e1ed450aee68daba074281a97
Mukarram685 <muka654r@gmail.com>
2026-05-13 16:29:05 +0500

    update server listen logic for non-production environments and enhance MongoDB error handling

--- src/DB/Connection.js
+++ src/DB/Connection.js
@@ -7,8 +7,7 @@ const ConnectDB = async () => {
         console.log(`MongoDB Connected: ${connect.connection.host}`);
 
     } catch (err) {
-        console.log(err);
-        process.exit(1);
+        console.error("MongoDB Connection Error:", err.message);
     }
 
 }
