a5a5c2c55f9629363779ced652652726c3953571
Mukarram685 <muka654r@gmail.com>
2026-05-14 19:48:01 +0500

    correcting vercel file and index for deploy

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ app.use((err, req, res, next) => {
     });
 });
 
-if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+// if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     });
+// }
 
 export default app;
