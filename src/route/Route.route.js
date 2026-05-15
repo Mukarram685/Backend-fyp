4d73e0ad562b0b86ced6ce5f084e447b088ae30b
Mukarram685 <muka654r@gmail.com>
2026-05-15 12:32:18 +0500

    route file fix

--- src/route/Route.route.js
+++ src/route/Route.route.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import { protect, authorizeRoles } from '../middleware/Auth.midleware.js';
-import {
-  createRoute,
-  getCompanyRoutes,
-  getRouteById,
-  updateRoute,
-  deleteRoute
-} from '../controller/Route/Route.controller.js';
-import { validateScope } from '../middleware/RBAC.middleware.js';
-
-const BusRoute = express.Router();
-
-BusRoute.use(protect);
-
-BusRoute.use(authorizeRoles('companyadmin', 'superadmin'));
-
-BusRoute.post('/createRoute', createRoute);
-BusRoute.get('/allRoutes', getCompanyRoutes);
-BusRoute.get('/getRouteByCompany/:id', validateScope('route', 'view'), getRouteById);
-BusRoute.patch('/updateRoute/:id', validateScope('route', 'manage'), updateRoute);
-BusRoute.delete('/deleteRoute/:id', validateScope('route', 'manage'), deleteRoute);
-
-=======
 import express from 'express';
 import { protect, authorizeRoles } from '../middleware/Auth.midleware.js';
 import {
@@ -45,5 +20,4 @@ BusRoute.get('/getRouteByCompany/:id', getRouteById);
 BusRoute.patch('/updateRoute/:id', updateRoute);
 BusRoute.delete('/deleteRoute/:id', deleteRoute);
 
->>>>>>> 6cd5ccecbe2dd260873edaf9dbf5f315b9bd5afa
 export default BusRoute;
