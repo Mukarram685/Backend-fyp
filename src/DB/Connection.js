163d6f60d51cb43a2c1f88586fcd037bf7cf5920
Mukarram685 <muka654r@gmail.com>
2026-07-01 16:12:59 +0500

    dns

--- src/DB/Connection.js
+++ src/DB/Connection.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
-// import dns from "dns";
+import dns from "dns";
 
-// dns.setServers(["8.8.8.8", "1.1.1.1"]);
+if (process.env.NODE_ENV !== "production") {
+    dns.setServers(["8.8.8.8", "1.1.1.1"]);
+}
 
 const ConnectDB = async () => {
 
